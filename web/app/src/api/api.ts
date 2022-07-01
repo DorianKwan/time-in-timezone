0aa4a5b26f17b89304af6891e602db730872c020
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-06-30 23:04:38 -0600

    feat: setup FE api handler for time by timezone

--- web/app/src/api/api.ts
+++ web/app/src/api/api.ts
@@ -27,6 +27,26 @@ const createApi = (baseUrl: string) => {
 
         return data;
       },
+      async getTimezoneTime(timezone: string) {
+        type Response = AxiosResponse<ApiResponse<ApiData.TimezoneTime>>;
+
+        const response = await coreServiceApi.get<Response>(
+          '/world-time-api/timezone-time',
+          {
+            params: timezone,
+            // must stop axios from encoding the forward slashes
+            paramsSerializer: (params: string) => {
+              return `timezone=${params}`;
+            },
+          },
+        );
+
+        if (response.status !== 200) throw new Error(response.statusText);
+
+        const { data } = response.data;
+
+        return data;
+      },
     },
   };
 };
